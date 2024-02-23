06cbeb415a314123bcb34c1c9075bb17c74a2187
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-02-23 21:08:57 +0530

    change

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,33 +1,12 @@
-// let firebaseConfig = {
-//     apiKey: "AIzaSyCP0COzwhPdXJiG3iYRwoKR5Y0W81ztcCs",
-//     authDomain: "blogging-website-28de1.firebaseapp.com",
-//     projectId: "blogging-website-28de1",
-//     storageBucket: "blogging-website-28de1.appspot.com",
-//     messagingSenderId: "696448260462",
-//     appId: "1:696448260462:web:e379c618646771dd23c97f"
-//   };
+let firebaseConfig = {
+    apiKey: "AIzaSyCP0COzwhPdXJiG3iYRwoKR5Y0W81ztcCs",
+    authDomain: "blogging-website-28de1.firebaseapp.com",
+    projectId: "blogging-website-28de1",
+    storageBucket: "blogging-website-28de1.appspot.com",
+    messagingSenderId: "696448260462",
+    appId: "1:696448260462:web:e379c618646771dd23c97f"
+  };
 
- 
-// firebase.initializeApp(firebaseConfig);
-// firebase.initializeApp(firebaseConfig);
+ firebase.initializeApp(firebaseConfig);
 
-// let db = firebase.firestore();
-
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCP0COzwhPdXJiG3iYRwoKR5Y0W81ztcCs",
-  authDomain: "blogging-website-28de1.firebaseapp.com",
-  projectId: "blogging-website-28de1",
-  storageBucket: "blogging-website-28de1.appspot.com",
-  messagingSenderId: "696448260462",
-  appId: "1:696448260462:web:e379c618646771dd23c97f"
-};
-
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+let db = firebase.firestore();
